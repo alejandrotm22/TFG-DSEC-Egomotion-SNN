494b5e469c027e2e0621835cf3bfd16626840a61
Alejandro Torres Martínez <torresmartinezalejandro2@gmail.com>
2025-03-27 19:25:27 +0100

    Nueva obtencion del mapa rectificado y visualizacion de vel y omega

--- obtener_gt.py
+++ obtener_gt.py
@@ -211,52 +211,73 @@ def generate_optical_flow_video(Dx, Dy, fps, filename='optical_flow.mp4', vmax=2
     print(f"Video guardado como {filename}")
 
 if __name__ == '__main__':
-    
+    # Directorio global
+    global_path = "./zurich_city_02_d"
     # Nombre de los directorios
-    calibration_path = "./calibration"
-    depth_path = "./depth"
-    disparity_path = "./disparity_event"
-    of_path = "./forward"
-    gt_tensors_path = "./gt_tensors"
-    rectified_images_path = "./images_rectified"
-    mask_path = "./mask_tensors"
-    events_path = "./events/left"
+    calibration_path = os.path.join(global_path, "calibration")
+    disparity_path = os.path.join(global_path, "disparity_event")
+    of_path = os.path.join(global_path, "flow", "forward")
+    events_path = os.path.join(global_path, "events", "left")
 
     # Nombre de los archivos
-    of_timestamps = "forward_timestamps.txt"
-    image_timestamps = "image_timestamps.txt"
-    dispartiy_timestamps = "disparity_timestamps.txt"
-    reprojection_matrix = "cam_to_cam.yaml"
+    of_timestamps = os.path.join(global_path, "forward_timestamps.txt")
+    dispartiy_timestamps = os.path.join(global_path, "disparity_timestamps.txt")
+    calibration = "cam_to_cam.yaml"
     rectify_map_file = "rectify_map.h5"
 
     # Listar y ordenar los ficheros de los directorios
-    depth_map_list = listar_ordenar(depth_path)
     disparity_map_list = listar_ordenar(disparity_path)
     of_list = listar_ordenar(of_path)
-    gt_tensors_list = listar_ordenar(gt_tensors_path)
-    rectified_images_list = listar_ordenar(rectified_images_path)
 
     # Cargar los archivos de los timestamps
     of_timestamps = np.loadtxt(of_timestamps, delimiter=',', dtype='int64')
     of_timestamps = of_timestamps[:, 1]
-    image_timestamps = np.loadtxt(image_timestamps, dtype='int64')
     dispartiy_timestamps = np.loadtxt(dispartiy_timestamps, dtype='int64')
 
     # Cargar la matriz de reproyección Q
-    with open(os.path.join(calibration_path, reprojection_matrix), "r") as file:
+    with open(os.path.join(calibration_path, calibration), "r") as file:
         data = yaml.safe_load(file)
 
-    Q = np.array(data["disparity_to_depth"]["cams_12"])
+    Q = np.array(data["disparity_to_depth"]["cams_03"])
 
-    # Cargar la matriz de rectificación
-    rectmap_file = h5py.File(os.path.join(events_path, rectify_map_file))
-    rectmap = rectmap_file['rectify_map'][()]
+    # # Cargar la matriz de rectificación del propio dataset
+    # rectmap_file = h5py.File(os.path.join(events_path, rectify_map_file))
+    # rectmap = rectmap_file['rectify_map'][()]
+    #
+    # print(f"El shape de rectmap es {rectmap.shape}")
 
-    print(f"El shape de rectmap es {rectmap.shape}")
+    ### Crear la matriz de rectificación a partir de los datos de calibración del dataset ###
+    camera_matrix = np.array([
+        [data["intrinsics"]["cam0"]["camera_matrix"][0],             0.0, data["intrinsics"]["cam0"]["camera_matrix"][2]],
+        [            0.0, data["intrinsics"]["cam0"]["camera_matrix"][1], data["intrinsics"]["cam0"]["camera_matrix"][3]],
+        [            0.0,             0.0,             1.0]
+    ])
 
-    dx_frames = []
-    dy_frames = []
-    mask_optflow = []
+    distortion_coeffs = np.array(data["intrinsics"]["cam0"]["distortion_coeffs"])
+
+    # Compute undistorted image plane.
+    dist_x_map, dist_y_map = np.meshgrid(np.arange(data["intrinsics"]["cam0"]["resolution"][0], dtype=np.float32),
+                                         np.arange(data["intrinsics"]["cam0"]["resolution"][1], dtype=np.float32))
+    dist_x_map    = dist_x_map.flatten()
+    dist_y_map    = dist_y_map.flatten()
+    dist_xy_map   = np.stack((dist_x_map[:, np.newaxis], dist_y_map[:, np.newaxis]), axis=2)
+
+    undist_xy_map = cv2.undistortPoints(dist_xy_map, camera_matrix, distortion_coeffs)
+    undist_x_map  = undist_xy_map[:, :, 0].flatten().reshape(480, 640)
+    undist_y_map  = undist_xy_map[:, :, 1].flatten().reshape(480, 640)
+
+    print(f"Undistorted image plane shape: {undist_y_map.shape}")
+    print(f"Undistorted image plane shape: {undist_x_map.shape}")
+
+    # Necesario para crear el video
+    # dx_frames = []
+    # dy_frames = []
+    # mask_optflow = []
+
+    vel_x, vel_y, vel_z = [], [], []
+    omega_x, omega_y, omega_z = [], [], []
+    plt.ion()  # Habilitar el modo interactivo de Matplotlib
+    fig, axs = plt.subplots(2, 1, figsize=(10, 6))  # Dos subgráficos: uno para Vel y otro para Omega
 
     #for i in range(32, 33):
     for i, file in enumerate(of_list):
@@ -266,58 +287,91 @@ if __name__ == '__main__':
         # timestamp para el que se va a obtener la gt
         timestamp = of_timestamps[i]
 
-        #### Obtenemos los optical flow ground truth ####
+        #### Obtenemos los optical flow ground truth y su máscara ####
         Dx, Dy, mask_of = load_optical_flow(of_path, of_list[i])
+        print(f"Shape del optical flow de X: {Dx.shape}")
+
+        # Creo la mascara de donde X e Y en el optical flow son 0's
+        mask_of_zeros = (Dx != 0.0) & (Dy != 0.0)
 
         # Código usado para generar el video
         # if i == 0: # Uso siempre la misma máscara por simplificar
         #     mask_optflow = mask_of
         # ux = Dx
         # yx = Dy
-        # ux[~mask_of] = 0
-        # yx[~mask_of] = 0
+        # # ux[~mask_of] = 0
+        # # yx[~mask_of] = 0
         # dx_frames.append(ux)
         # dy_frames.append(yx)
-
-        print(f"Shape del optical flow de X: {Dx.shape}")
-        mask_of_zeros = (Dx != 0.0) & (Dy != 0.0)
-
-
 
         # Indices para las imagenes y la disparidad que tienen el mismo timestamp
         index_disparity = np.where(dispartiy_timestamps == timestamp)[0][0]
 
         #Cargamos la profunidad
         depth, mask_d = compute_depth_from_disparity(disparity_path, disparity_map_list[index_disparity], Q)
-        # Unir las máscaras con una operación lógica AND
-        mask_combined = mask_d & mask_of
-        mask_combined = mask_combined & mask_of_zeros
-        #Codigo para mostrar las imagenes de profundidad
+
+        # Unir las máscaras de disparidad, optical flow y ceros
+        mask_combined = mask_d & mask_of & mask_of_zeros
+
+        # #Codigo para mostrar las imagenes de profundidad
         # cv2.imshow("imagen",disp_img_to_rgb_img(depth))
         # cv2.waitKey(0)
 
+        # Obtener la inversa de la profundidad
         print(f"Shape de la profundidad: {depth.shape}")
         invZ = 1.0 / depth[mask_combined]
         print(f"Shape de la inversa de la profundidad: {invZ.shape}")
 
+        # Le pasamos la máscara al óptical flow
         Dx = Dx[mask_combined]
         Dy = Dy[mask_combined]
 
-        if np.any(Dx == 0.0) and np.any(Dy == 0.0):
-            print("El optical flow contiene valores 0.0")
-        else:
-            print("No hay valores 0.0 en el optical flow")
-        print(f"Shape de Dx: {Dx.shape}")
+        # Código para obtener X e Y a partir de la matriz rectificada
+        # coordenadas = np.array(np.where(mask_combined))
+        # X = (rectmap[coordenadas[0], coordenadas[1], 0] - 335.0999870300293) / 569.7632987676102
+        # Y = (rectmap[coordenadas[0], coordenadas[1], 1] - 221.23667526245117) / 569.7632987676102
 
-        coordenadas = np.array(np.where(mask_combined))
-        X = rectmap[coordenadas[0], coordenadas[1], 0]
-        Y = rectmap[coordenadas[0], coordenadas[1], 1]
+        # Le pasamos la máscara a los valores de X e Y rectificados
+        X = undist_x_map[mask_combined]
+        Y = undist_y_map[mask_combined]
+
         print(f"Los valores unique de X es de: {np.unique(X)}")
         print(f"Los valores unique de Y es de: {np.unique(Y)}")
 
-
+        # Calculamos el ego motion a partir de los datos
         ego_motion = est_self_motion_given_depth_normal_flow(X, Y, Dx, Dy, invZ)
         print(ego_motion)
+
+        # Extraer valores de Vel y Omega
+        vel_x.append(ego_motion['Vel'][0])
+        vel_y.append(ego_motion['Vel'][1])
+        vel_z.append(ego_motion['Vel'][2])
+
+        omega_x.append(ego_motion['Omega'][0])
+        omega_y.append(ego_motion['Omega'][1])
+        omega_z.append(ego_motion['Omega'][2])
+
+        # Limpiar y actualizar el gráfico
+        axs[0].cla()
+        axs[0].plot(vel_x, label='Vel X', color='r')
+        axs[0].plot(vel_y, label='Vel Y', color='g')
+        axs[0].plot(vel_z, label='Vel Z', color='b')
+        axs[0].set_title("Velocidad (Vel)")
+        axs[0].legend()
+        axs[0].set_ylabel("Valor")
+
+        axs[1].cla()
+        axs[1].plot(omega_x, label='Omega X', color='r')
+        axs[1].plot(omega_y, label='Omega Y', color='g')
+        axs[1].plot(omega_z, label='Omega Z', color='b')
+        axs[1].set_title("Velocidad Angular (Omega)")
+        axs[1].legend()
+        axs[1].set_ylabel("Valor")
+
+        plt.pause(0.1)  # Pausar para visualizar los cambios en tiempo real
+
+    plt.ioff()  # Desactivar el modo interactivo
+    plt.show()  # Mostrar la gráfica final
 
     # Continuación del código usado para generar el vídeo
     # dx_frames = np.stack(dx_frames, axis=0)
